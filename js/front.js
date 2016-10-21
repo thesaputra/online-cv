3f211505b948dc67780ea68f99d75107f36e2ce9
ajisaputra <aji.saputra@kiranatama.com>
2016-10-21 17:55:07 +0700

    update meta

--- js/front.js
+++ js/front.js
@@ -113,7 +113,7 @@ function map() {
             var contentString = '<div class="info-window">' +
                     '<h3>Info Window Content</h3>' +
                     '<div class="info-content">' +
-                    '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>' +
+                    '<p>I am here</p>' +
                     '</div>' +
                     '</div>';
 
